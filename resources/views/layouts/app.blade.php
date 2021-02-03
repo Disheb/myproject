8c0b22de2448af6893c862b6e9dd91d1865c0e06
makhmudovazeez <azeezmakhmudov@gmail.com>
2021-02-03 15:31:35 +0500

    body color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title-block')</title>
-    <link rel="stylesheet" href="css/firstcss.css">
+    <link rel="stylesheet" href="{{asset('css/firstcss.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<body>
+<body class="body">
 @include('inc.header')
 @if(Request::is('/'))
 @include('inc.hero')
